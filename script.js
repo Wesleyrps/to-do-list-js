253ec59d8fe857b1d68cdf0a19235960605841e6
Wesley Rodrigues <wesleyrps10@gmail.com>
2024-02-23 01:03:33 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+const localStorageKey = 'to-do-list-ws'
+
+function validateIfExistsNewTask()
+{
+    let values      = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let inputValue  = document.getElementById('input-new-task').value
+    let exists      = values.find(x => x.name == inputValue)
+    return !exists ? false : true
+}
+
+function newTask()
+{
+    let input = document.getElementById('input-new-task')
+    input.style.border = ''
+
+    // Validation
+    if(!input.value)
+    {
+        input.style.border = '1px solid red'
+        alert('Digite algo para inserir em sua lista')
+    }
+    else if(validateIfExistsNewTask())
+    {
+        alert('Já existe uma task com essa descrição')
+    }
+    else
+    {
+        // Increment to localStorage
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+        values.push({
+            name: input.value
+        })
+        localStorage.setItem(localStorageKey,JSON.stringify(values))
+        showValues()
+    }
+    input.value = ''
+}
+
+function showValues()
+{
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let list = document.getElementById('to-do-list')
+    list.innerHTML = ''
+    for(let i = 0; i < values.length; i++)
+    {
+        list.innerHTML +=   `<li id='li-list-tasks'>${values[i]['name']}
+                                <button id='btn-edit' onclick='editItem("${values[i]['name']}")'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                                    </svg>
+                                </button>
+        
+                                <button id='btn-ok' onclick='removeItem("${values[i]['name']}")'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                                    </svg>
+                                </button>
+                            </li>`
+    }
+}
+
+function editItem(data)
+{
+    alert(`editando tarefa ${data}`)
+}
+
+function removeItem(data)
+{
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let index = values.findIndex(x => x.name == data)
+    values.splice(index,1)
+    localStorage.setItem(localStorageKey,JSON.stringify(values))
+    showValues()
+}
+
+showValues()
